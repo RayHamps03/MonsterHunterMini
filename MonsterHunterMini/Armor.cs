39e1851b23ad661ed3c7c02841398e0982f64a1f
UltProgrammerJune <jsimeon8620@students.cptc.edu>
2026-02-07 23:55:50 -0800

    Add Armor and Weapons classes with basic properties
    Added Armor.cs and Weapons.cs defining internal Armor and Weapons classes in the MonsterHunterMini namespace. Each class includes properties for name, unique ID, stat (defense or attack), and optional materials array. Constructors and XML documentation comments are provided.

---
+++ MonsterHunterMini/Armor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterHunterMini;
+
+internal class Armor
+{
+    public Armor(string armorName, int armorDefStat)
+    {
+        Name = armorName;
+        Defense = armorDefStat;
+    }
+
+    /// <summary>
+    /// Name of the armor set.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The Unique Identifier for the armor set.
+    /// </summary>
+    public int weaponID { get; set; }
+
+    /// <summary>
+    /// The attack stat of the weapon, which determines 
+    /// how much damage it can deal to monsters.
+    /// </summary>
+    public int Defense { get; set; }
+
+    public Material[]? materials { get; set; }
+
+}
---
+++ MonsterHunterMini/Weapons.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterHunterMini;
+
+internal class Weapons
+{
+    public Weapons(string weaponName, int weaponAttStat)
+    {
+        Name = weaponName;
+        Attack = weaponAttStat;
+    }
+
+    /// <summary>
+    /// Name of the weapon.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The Unique Identifier for the weapon.
+    /// </summary>
+    public int weaponID { get; set; }
+
+    /// <summary>
+    /// The attack stat of the weapon, which determines 
+    /// how much damage it can deal to monsters.
+    /// </summary>
+    public int Attack { get; set; }
+
+    public Material[]? materials { get; set; }
+
+}
